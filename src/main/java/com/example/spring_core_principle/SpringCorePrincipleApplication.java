4b35be94f8dc754e5e15554751b397e2377e251a
eunjis <ejejc@naver.com>
2023-03-02 22:20:54 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_core_principle/SpringCorePrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_core_principle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorePrincipleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCorePrincipleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_core_principle/SpringCorePrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_core_principle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrincipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

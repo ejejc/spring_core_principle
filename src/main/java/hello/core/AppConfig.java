0fc6a060cd0101daf8340d2344286aaee86df1db
eunjis <ejejc@naver.com>
2023-03-06 22:55:55 +0900

    Loc, DI 컨테이너 및 스프링 전환.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 실제 동작에 필요한 구현 객체를 각 client가 아닌 AppConfig에서 생성한다.
@@ -15,8 +17,10 @@ import hello.core.order.OrderServiceImpl;
  * OCP : 변경에는 닫혀있고 확장에는 열려 있다 > 만족 : 정책이 변경된다고 해도 client는 변경되지 않고 확장할 수 있다.
  * SRP : 한 클래스는 하나의 책임만 가지고 있어야 한다. > 기존에는 직접 구현 객체를 생성하고 실행하는 다양한 책임을 가지고 있었지만 AppConfig의 등장으로 단순히 실행만 하는 역활만 담당 > 만족
  */
+@Configuration
 public class AppConfig {
 
+    @Bean // 빈 이름은 메소드 이름으로 등록된다.
     public MemberService memberService() {
         // 생성자 주입 - 구현체를 client가 아닌 별도 클래스에서 넣어준다.
         /**
@@ -26,16 +30,19 @@ public class AppConfig {
     }
 
     // 생성해주는 역활을 분리하자. - 중복도 제거됨 + 역활과 구현이 한눈에 보여짐!
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         // 생성자 주입
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 생성해주는 역활을 분리하자.
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,12 +5,17 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);

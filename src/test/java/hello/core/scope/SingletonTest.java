ef185bcdbd9a999f47bc0c3d47d6010a7f49527b
eunjis <ejejc@naver.com>
2023-04-10 20:51:47 +0900

    빈스코프와 프로토타입 스코프.

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("singletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("singletonBean.destroy");
+        }
+    }
+}
